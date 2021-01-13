cc056ac2fef02621826d4b84317ce02cf484737e
VEBER Arnaud <arnaud@veber.pw>
2021-01-13 02:25:17 +0100

    feat(query): query topic

--- src/types.ts
+++ src/types.ts
@@ -3,10 +3,12 @@ import { DataQuery, DataSourceJsonData } from '@grafana/data';
 export interface MyQuery extends DataQuery {
   queryText?: string;
   constant: number;
+  topic: string;
 }
 
 export const defaultQuery: Partial<MyQuery> = {
   constant: 6.5,
+  topic: '',
 };
 
 /**
